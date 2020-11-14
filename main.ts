f1172f2086fdac0f24bc3ffaa910c1db5e0d5983
Andreas Böhrnsen <deepflame@users.noreply.github.com>
2020-11-14 15:35:41 +0100

    add basic remote with second microbit

--- main.ts
+++ main.ts
@@ -1,21 +1,69 @@
 input.onButtonPressed(Button.A, function () {
-    radio.sendString("yes")
-    basic.showIcon(IconNames.Yes)
+    radio.sendString("left")
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # . # # #
+        . # . . .
+        . . # . .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("straight")
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
     basic.pause(500)
     basic.clearScreen()
 })
 radio.onReceivedString(function (receivedString) {
-    if (receivedString == "yes") {
-        basic.showIcon(IconNames.Yes)
-    } else if (receivedString == "no") {
-        basic.showIcon(IconNames.No)
+    if (receivedString == "left") {
+        Rover.MotorRunDual(0, 50)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # . # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (receivedString == "right") {
+        Rover.MotorRunDual(50, 0)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # . #
+            . . . # .
+            . . # . .
+            `)
+    } else if (receivedString == "straight") {
+        Rover.MotorRunDual(50, 50)
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
     }
     basic.pause(500)
     basic.clearScreen()
+    Rover.MotorStopAll(MotorActions.Stop)
 })
 input.onButtonPressed(Button.B, function () {
-    radio.sendString("no")
-    basic.showIcon(IconNames.No)
+    radio.sendString("right")
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # . #
+        . . . # .
+        . . # . .
+        `)
     basic.pause(500)
     basic.clearScreen()
 })
